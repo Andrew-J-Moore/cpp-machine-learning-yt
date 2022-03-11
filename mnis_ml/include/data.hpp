d5b688dcca5d6db3fd659fff8ebfbb89287eaed9
Andrew Moore <andrew.moore9497@gmail.com>
2022-03-11 14:54:38 -0600

    24:00 in part one on YT

--- mnis_ml/include/data.hpp
+++ mnis_ml/include/data.hpp
@@ -6,10 +6,22 @@
 #include "stdint.h"
 #include "stdio.h"
 
-class Data {
-    std::vector<uint8_t> *feature_vector;
+class data {
+    std::vector<uint8_t> *feature_vector; // no class at end
     uint8_t label;
-    int enumerated;
+    int enum_label;
+
+    public: 
+        void set_feature_vector(std::vector<uint8_t> *);
+        void append_to_feature_vector(uint8_t);
+        void set_label(uint8_t);
+        void set_enumerated_label(int);
+
+        int get_feature_vector_size();
+        uint8_t get_label();
+        uint8_t get_enumerated_label();
+
+        std::vector<uint8_t> * get_feature_vector();
 };
 
 
